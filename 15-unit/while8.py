1a24d94828ea61c56f6614f1ea5918871685d249
Shermukhammad Kayumov <shermuhammad.qayumov.02@mail.ru>
2024-01-09 17:04:08 +0500

    while8

--- 15-unit/while8.py
+++ 15-unit/while8.py
@@ -3,7 +3,6 @@ while n<=0:
     print("n value must be bigger than 0. Enter valud number!")
     n = int(input("Enter n value (n > 0): "))
 k = 1
-while not k**2>n:
+while not k**2<=n:
     k += 1
-result = k**2
-print(f"Given number is {n} and  (k**2 > n) second number is {k}, the power of k = {result} is bigger than {n}")
+print(k)
